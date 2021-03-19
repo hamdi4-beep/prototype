e66d40d1bcb8dfdad394080e160519c7b453a774
hamdi-beep4 <git@github.com>
2021-03-19 05:39:15 -0700

    improved AI

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const colors = ["green", "blue", "white", "pink",
                 "purple", "cyan", "silver", "crimson"];
 
 document.querySelector("input").addEventListener("keyup", function(e) {
+    e.target.value == "" ? title.textContent : title.textContent = this.value;
     colors.forEach(function(color) {
         if (e.target.value.toLowerCase().indexOf(color) != -1) {
             title.style.color = color;
+        } else if (e.target.value.toLowerCase() == "default") {
+            title.textContent = "Hamdi Kamel";
+            title.style.color = "white";
         }
     });
 });
+
+title.onclick = function() {
+    this.textContent = "Welcome, User!";
+}
